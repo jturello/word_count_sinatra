3113bed1c9f3f2aec807f9e1977dd01938b6c4aa
James Turello <jturello@yahoo.com>
2016-07-01 14:43:55 -0700

    add integration spec - Welcome to Word Count page title text is wrapped in an h1 tag

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -3,15 +3,16 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe("word_count homepage form input behavior", {:type => :feature}) do
+describe("word_count homepage form input fields and functionality", {:type => :feature}) do
 
-  it("displays text 'Welcome to Word Count' on form input homepage result - 1 occurrence of word - when user enters the same word (case insensitive) in text and word to count fields") do
+  it('displays text: Welcome to Word Count') do
     visit('/')
-    # fill_in('text', :with => 'SOMETHING')
-    # fill_in('word_to_count', :with => 'sOmEtHiNg')
-    # click_button('Enter')
-    # save_and_open_page
     expect(page).to have_content('Welcome to Word Count')
+  end
+
+  it('wraps Welcome to Word Count page title text in h1 tag') do
+    visit('/')
+    expect(page.find('h1', :text => 'Welcome to Word Count'))
   end
 
   # it('displays result - 1 occurrence of word - when user enters the same word (case insensitive) in text and word to count fields') do
@@ -22,4 +23,5 @@ describe("word_count homepage form input behavior", {:type => :feature}) do
   #   # save_and_open_page
   #   expect(page).to have_content('Number of occurrences of word-to-count in text: 1')
   # end
+
 end
