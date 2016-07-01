8f5187dde9632db8b8ef25a06e1169e27815a230
James Turello <jturello@yahoo.com>
2016-07-01 15:54:02 -0700

    update app.rb with correct routes and code

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
-require('./lib/TEMPLATE')
+require('./lib/word_count')
 require('pry')
 require('launchy')
 
@@ -10,15 +10,9 @@ get('/') do
   erb(:form)
 end
 
-get('/RESULT') do
-  @err = nil
-
-  if Integer(params['TEMPLATE'].TEMPLATE()) == SOMETHING
-    @err = "Argument not valid - #{params['TEMPLATE']}"
-    erb(:RESULT)
-  else
-    @VAR = params['TEMPLATE'].to_i.TEMPLATE()
-    erb(:RESULT)
-  end
-
+get('/word_count') do
+    @text = params['text']
+    @word_to_count = params['word_to_count']
+    @count = @text.word_count(@word_to_count)
+    erb(:word_count)
 end
