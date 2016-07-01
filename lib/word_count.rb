fc86217009763798c4547b276e18b98d641e185c
James Turello <jturello@yahoo.com>
2016-07-01 10:17:00 -0700

    add first passing unit test - return 1 when text and word are the same

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,5 +1,7 @@
 class String
   define_method(:word_count) do |word|
+    
+    self.scan(/\b#{word}\b/).size
 
   end
 end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -3,7 +3,7 @@ require('word_count')
 
 describe('String#word_count') do
 
-  it('returns 1 when the word and text are the same single word') do
+  it('returns 1 when the word (="word") and text (="word") are the same single word') do
     expect(("word").word_count("word")).to(eq(1))
   end
 
